cea2a8b0f6908a33370c9dea3dd629adcdc64a80
rakhiknowlge <myknowlge@gmail.com>
2021-11-10 09:37:17 +0530

    new commit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 print("my name is")
-print("i live in ujjain")
+print("Jai shree mahakal")
 
 
 def print_hi(name):
